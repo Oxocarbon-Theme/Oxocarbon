8c2960935b8234083239d94111ae7ff1563bd83b
Lynxie <ImLynxie@proton.me>
2024-02-21 11:05:01 -0800

    Initial Commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 1.0.0 - 2024-02-21
+- Initial Commit w/ ready to release plugin theme
+- Released plugin on Jetbrains Marketplace 🎉🪅🥳
---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+import org.jetbrains.changelog.Changelog
+import org.jetbrains.changelog.date
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+fun properties(key: String) = project.findProperty(key).toString()
+
+plugins {
+    // Java Support
+    id("java")
+    // Kotlin support
+    id("org.jetbrains.kotlin.jvm") version "1.9.21"
+    // Gradle IntelliJ Plugin
+    id("org.jetbrains.intellij") version "1.16.1"
+    // Gradle Changelog Plugin
+    id("org.jetbrains.changelog") version "2.0.0"
+}
+
+group = properties("pluginGroup")
+version = properties("pluginVersion")
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    pluginName.set(properties("pluginName"))
+    version.set(properties("platformVersion"))
+    type.set(properties("platformType")) // Target IDE Platform
+
+    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+}
+
+// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
+changelog {
+    version.set(properties("pluginVersion"))
+    path.set(file("CHANGELOG.md").canonicalPath)
+    header.set(provider { "${version.get()} - ${date()}"})
+    headerParserRegex.set("""(\d\.\d\.\d)""".toRegex())
+    itemPrefix.set("-")
+    keepUnreleasedSection.set(false)
+    unreleasedTerm.set("[Unreleased]")
+    groups.set(listOf("Added", "Changed", "Deprecated", "Removed", "Fixed", "Security"))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    properties("javaVersion").let {
+        withType<JavaCompile> {
+            sourceCompatibility = it
+            targetCompatibility = it
+        }
+
+        withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = it
+        }
+    }
+
+
+    patchPluginXml {
+        version.set(properties("pluginVersion"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
+
+        changeNotes.set(provider {
+            changelog.renderItem(changelog.getLatest(), Changelog.OutputType.HTML)
+        })
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
