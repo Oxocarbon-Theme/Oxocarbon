cce573959fb300176b05d599223e9fdefb9ec02e
poach3r <n0nam3b01@gmail.com>
2024-11-27 15:23:26 +0000

    Update Gradle configuration to be compatible with modern IDEA versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.9.21"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.16.1"
+    id("org.jetbrains.intellij") version "1.17.4"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.0.0"
 }
